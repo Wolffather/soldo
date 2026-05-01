423024e3881e49dd02911cbb0f9f183ceddba0e7
Wolffather <maxcross93@gmail.com>
2026-05-01 23:27:34 +0300

    Fix tg token saving

--- src/main/java/ru/savvy/soldo/tenant/TenantConfigRepository.java
+++ src/main/java/ru/savvy/soldo/tenant/TenantConfigRepository.java
@@ -8,7 +8,7 @@ import ru.savvy.soldo.tenant.model.TenantConfig;
 
 public interface TenantConfigRepository extends JpaRepository<TenantConfig, Long> {
 
-    @Modifying
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("UPDATE TenantConfig c SET c.telegramBotToken = :token, c.telegramBotUsername = null, " +
            "c.telegramWebhookSecret = CASE WHEN c.telegramWebhookSecret IS NULL THEN :secret ELSE c.telegramWebhookSecret END " +
            "WHERE c.tenantId = :tenantId")
@@ -16,7 +16,7 @@ public interface TenantConfigRepository extends JpaRepository<TenantConfig, Long
                            @Param("token") String token,
                            @Param("secret") String secret);
 
-    @Modifying
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("UPDATE TenantConfig c SET c.telegramBotToken = null, c.telegramBotUsername = null WHERE c.tenantId = :tenantId")
     void clearTelegramBot(@Param("tenantId") Long tenantId);
 }
