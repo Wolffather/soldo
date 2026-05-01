2ed5c748674ff2e0c35cf5ca5fa366b3632a4338
Wolffather <maxcross93@gmail.com>
2026-05-02 00:08:50 +0300

    Fix webhook controller

--- src/main/java/ru/savvy/soldo/bot/controller/TelegramWebhookController.java
+++ src/main/java/ru/savvy/soldo/bot/controller/TelegramWebhookController.java
@@ -3,6 +3,7 @@ package ru.savvy.soldo.bot.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +27,11 @@ import java.util.Map;
 public class TelegramWebhookController {
 
     private final TelegramWebhookService telegramWebhookService;
+
+    @GetMapping("/{tenantSlug}")
+    public ResponseEntity<Void> healthCheck(@PathVariable String tenantSlug) {
+        return ResponseEntity.ok().build();
+    }
 
     @PostMapping("/{tenantSlug}")
     public ResponseEntity<Void> handleUpdate(
